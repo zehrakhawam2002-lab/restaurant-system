b8478d0b5a43654352ebcc6e24fa1eeaa488e511
zahraalizahra <159138572+zahraalizahra@users.noreply.github.com>
2026-05-27 20:38:52 +0300

    debug db connection

--- db/index.js
+++ db/index.js
@@ -1,15 +1,6 @@
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL || process.env.DATABASE_URL,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 5000,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-});
-pool.on('error', (err) => {
-  console.error('[DB] Unexpected error on idle client', err.message);
-});
-module.exports = {
-  query:     (text, params) => pool.query(text, params),
-  getClient: () => pool.connect(),
-};
+const { Pool } = require('pg'); 
+const connectionString = process.env.POSTGRES_URL || process.env.DATABASE_URL; 
+console.log('[DB] Using:', connectionString?.split('@')[1]); 
+const pool = new Pool({ connectionString, max: 20, ssl: { rejectUnauthorized: false } }); 
+pool.on('error', (err) => console.error('[DB] Error:', err.message)); 
+module.exports = { query: (text, params) => pool.query(text, params), getClient: () => pool.connect() }; 
